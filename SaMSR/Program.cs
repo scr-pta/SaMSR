514cadbd32095b905b4e5f848a2341f6a392b052
Josh <joshuasawyerphone@gmail.com>
2019-08-22 22:10:42 +0100

    Added error tracking code

--- SaMSR/Program.cs
+++ SaMSR/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Sentry;
 
 namespace SaMSR
 {
@@ -14,9 +15,12 @@ namespace SaMSR
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            using (SentrySdk.Init("https://a8be7a6e66364a4d827aba202dd3e0ec@sentry.io/1539264"))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
         }
     }
 }
