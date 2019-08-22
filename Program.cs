95cb110c24d803ac5ecff79341b2f6e690f281fd
Josh <joshuasawyerphone@gmail.com>
2019-08-22 19:55:59 +0100

    Added CLI files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SaMSR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
