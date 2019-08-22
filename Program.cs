81a051eda08e1d2401de54559c29d3bc6036e34b
Josh <joshuasawyerphone@gmail.com>
2019-08-22 22:01:34 +0100

    Added error tracking code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using Sentry;
 
 namespace SaMSR
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            using (SentrySdk.Init("https://a8be7a6e66364a4d827aba202dd3e0ec@sentry.io/1539264"))
+            {
+                Entry();
+            }
+        }
+
+        static void Entry()
         {
             Console.WriteLine("Hello World!");
         }
